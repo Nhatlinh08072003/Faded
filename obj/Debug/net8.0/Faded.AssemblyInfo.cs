60edcabd9432496c009e41f4eb42de2603d5a146
Nhatlinh08072003 <letiennhatlinh08072003@gmail.com>
2024-08-05 01:58:10 +0700

    footer

--- obj/Debug/net8.0/Faded.AssemblyInfo.cs
+++ obj/Debug/net8.0/Faded.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Faded")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78c9fa524b4d4f53ebfc20df3e5b0769ebf0baa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Faded")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Faded")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
